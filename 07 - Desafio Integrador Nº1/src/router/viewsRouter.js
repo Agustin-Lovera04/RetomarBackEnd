dd75decdb96f737b9fcbda9e6bc970c8ad717d5b
Agustin <agusrugbyy@gmail.com>
2024-10-14 18:21:25 -0300

    Notebook 07

--- 07 - Desafio Integrador Nº1/src/router/viewsRouter.js
+++ 07 - Desafio Integrador Nº1/src/router/viewsRouter.js
@@ -6,6 +6,22 @@ export const router = Router();
 
 router.get('/',(req,res)=>{
     
-
     res.status(200).render('Home')
+})
+
+router.get('/products',async (req,res) =>{
+
+    let products
+
+    try {
+        products  = await productsModel.find().lean()
+        console.log(products);
+        
+        return res.status(200).render('products', {products})
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+
 })
