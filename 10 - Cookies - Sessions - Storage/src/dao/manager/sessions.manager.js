3a22742be0352d12a359949457fc304bb00abf7d
Agustin-Lovera04 <agusslovera04@gmail.com>
2024-12-24 17:54:08 -0300

    1º Commit

--- 10 - Cookies - Sessions - Storage/src/dao/manager/sessions.manager.js
+++ 10 - Cookies - Sessions - Storage/src/dao/manager/sessions.manager.js
@@ -7,13 +7,10 @@ export class SessionsManager{
         let success = true
         try {
             let user = await usersModel.findOne({_id: id})
-            if(!user){
-                return {success: false, error: `Internal Server Error - Contacte con un Administrador`}
-            }
+            if(!user){return {success: false, error: `Internal Server Error - Contacte con un Administrador`}}
+            
             return {success, user}
-        } catch (error) {
-            return {success: false, error: `Internal Server Error: ${error.message}}`}
-        }
+        } catch (error) {return {success: false, error: `Internal Server Error: ${error.message}}`}}
     }
 
 
@@ -21,35 +18,28 @@ export class SessionsManager{
         let success = true
         try {
             let existUser = await usersModel.findOne({email: email})
-            if(existUser){
-                return {success: false, error: `Ya existen usuarios con el email: ${email}`}
-            }
+            if(existUser){return {success: false, error: `Ya existen usuarios con el email: ${email}`}}
+            
             return {success}
-        } catch (error) {
-            return {success: false, error: `Internal Server Error: ${error.message}}`}
-        }
+
+        } catch (error) {return {success: false, error: `Internal Server Error: ${error.message}}`}}
     }
 
 
     async createUser(name, email, password, rol){
         let success = true
-
+        
         let userData = {name: name, email: email, password: password}
 
-        if(rol){
-           userData.rol = rol
-        }
+        if(rol){userData.rol = rol}
 
         try {
            let user =await usersModel.create(userData)
-           if(!user){
-            return {success: false, error: `Error Interno al registrar usuario - Contacte un Administrador`}
-           }
+           if(!user){return {success: false, error: `Error Interno al registrar usuario - Contacte un Administrador`}}
 
            return {success, user}
-        } catch (error) {
-            return {success: false, error: `Internal Server Error: ${error.message}}`}
-        }
+
+        } catch (error) {return {success: false, error: `Internal Server Error: ${error.message}}`}}
     }
 
 
@@ -59,14 +49,9 @@ export class SessionsManager{
 
         try {
             let existUser = await usersModel.findOne({email: email, password: password})
-            if(!existUser){
-                return {success: false, error: `Credenciales Incorrectas`}
-            }
+            if(!existUser){return {success: false, error: `Credenciales Incorrectas`}}
  
             return {success, existUser}
-        } catch (error) {
-            return {success: false, error: `Internal Server Error: ${error.message}}`}
-        }
+        } catch (error) {return {success: false, error: `Internal Server Error: ${error.message}}`}}
     }
-
 }
--- 10 - Cookies - Sessions - Storage/src/router/sessions.router.js
+++ 10 - Cookies - Sessions - Storage/src/router/sessions.router.js
@@ -7,63 +7,41 @@ export const router=Router()
 
 router.post('/register',async(req,res)=>{
     let {name, email, password} = req.body
-    if(!name || !email || !password) {
-        return res.redirect('/register?error=Debes completar todos los campos.')
-    }
+    if(!name || !email || !password) {return res.redirect('/register?error=Debes completar todos los campos.')}
 
     let exReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
     let valid = exReg.test(email)
-    if(valid === false){
-        return res.redirect('/register?error=Debes enviar un email valido.')
-    }
+    if(valid === false){return res.redirect('/register?error=Debes enviar un email valido.')}
 
     let existUser = await sessionsManager.searchUserInBD(email)
-    if(!existUser.success){
-        return res.redirect(`/register?error=${existUser.error}`)
-    }
+    if(!existUser.success){return res.redirect(`/register?error=${existUser.error}`)}
 
     let rol
-    if(email=== 'adminCoder@coder.com' && password === 'adminCod3r123'){
-        rol = 'Admin'
-    }
+    if(email=== 'adminCoder@coder.com' && password === 'adminCod3r123'){rol = 'Admin'}
 
     password = crypto.createHmac("sha256", "UDMV").update(password).digest("hex")
 
-    
     let user = await sessionsManager.createUser(name,email, password, rol)
-    if(!user.success){
-        return res.redirect(`/register?error=${user.error}`)
-    }
+    if(!user.success){return res.redirect(`/register?error=${user.error}`)}
 
     res.redirect('/login?message=Usuario Registrado Con Exito')
 })
 
 router.post('/login', async(req, res) =>{
     let {email, password} = req.body
-    if(!email || !password){
-        return res.redirect('/login?error=Debes completar todos los campos.')
-    }
+    if(!email || !password){return res.redirect('/login?error=Debes completar todos los campos.')}
 
     let exReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
     let valid = exReg.test(email)
-    if(valid === false){
-        return res.redirect('/login?error=Debes enviar un email valido.')
-    }
-
+    if(valid === false){return res.redirect('/login?error=Debes enviar un email valido.')}
 
     password = crypto.createHmac("sha256", "UDMV").update(password).digest("hex")
 
-
     let existUser = await sessionsManager.compareUserData(email, password)
-    if(!existUser.success){
-        return res.redirect(`/login?error=${existUser.error}`)
-    }
+    if(!existUser.success){return res.redirect(`/login?error=${existUser.error}`)}
 
 
-    req.session.user = {
-        name: existUser.existUser.name, email: email, rol: existUser.existUser.rol
-    }
-
+    req.session.user = {name: existUser.existUser.name, email: email, rol: existUser.existUser.rol}
 
     return res.redirect('/perfil')
 })
@@ -71,10 +49,7 @@ router.post('/login', async(req, res) =>{
 
 router.get('/logout', auth, async (req,res)=>{
     req.session.destroy(error =>{
-        if(error){
-            res.setHeader('Content-Type','application/json');
-            return res.status(500).json({InternalError: error});
-        }
+        if(error){ res.setHeader('Content-Type','application/json');return res.status(500).json({InternalError: error});}
     })
 
     return res.redirect('/login')
